92c91cd6796f14730f9e06e9b153088a761ca2be
Ksenia Ilinyh <ksenia.ilinyh@gmail.com>
2017-02-05 23:33:44 +0500

    Убрала console.log

--- js/colorize-element.js
+++ js/colorize-element.js
@@ -4,14 +4,8 @@ window.colorizeElement = function (element, colors, property) {
 
   function setRandomColor() {
     var currentColor = element.style[property];
-
-    console.log(currentColor + ' - currentColor');
-
     var newColor = colors[window.utils.getRandomElementExcept(colors, currentColor)];
     element.style[property] = newColor;
-
-    console.log(newColor + ' - newColor');
-    console.log('-----------');
   }
 
   element.addEventListener('click', function () {
